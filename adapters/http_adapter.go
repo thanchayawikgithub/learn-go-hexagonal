ff62c216e446675c5d394c19b909523b548938cd
thanchayawik <thanchayawik@gmail.com>
2024-09-08 16:20:17 +0700

    implemented http adapter

---
+++ adapters/http_adapter.go
@@ -0,0 +1,28 @@
+package adapters
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/thanchayawikgithub/learn-go-hexagonal/core"
+)
+
+type HttpOrderHandler struct {
+	service core.OrderService
+}
+
+func NewHttpOrderHandler(service core.OrderService) *HttpOrderHandler {
+	return &HttpOrderHandler{service}
+}
+
+func (h *HttpOrderHandler) CreateOrder(c *fiber.Ctx) error {
+	var order core.Order
+
+	if err := c.BodyParser(&order); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request"})
+	}
+
+	if err := h.service.CreateOrder(order); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(order)
+}
