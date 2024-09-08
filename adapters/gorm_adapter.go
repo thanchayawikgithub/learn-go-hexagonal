b407cbb2099c0440d7db2e1a7bb4c00ccf3179f2
thanchayawik <thanchayawik@gmail.com>
2024-09-08 16:12:32 +0700

    implemented gorm adapter

---
+++ adapters/gorm_adapter.go
@@ -0,0 +1,22 @@
+package adapters
+
+import (
+	"github.com/thanchayawikgithub/learn-go-hexagonal/core"
+	"gorm.io/gorm"
+)
+
+type GormOrderRepository struct {
+	db *gorm.DB
+}
+
+func NewGormOrderRepository(db *gorm.DB) core.OrderRepository {
+	return &GormOrderRepository{db}
+}
+
+func (r *GormOrderRepository) Save(order core.Order) error {
+	if result := r.db.Create(&order); result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/thanchayawikgithub/learn-go-hexagonal
 
 go 1.22.6
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	gorm.io/gorm v1.25.11 // indirect
+)
